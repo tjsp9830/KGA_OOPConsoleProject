6d3a278bf98e23f3ad3ffc4e2ae7793a32680eb5
tjsp9830 <mcgr1998@gmail.com>
2024-08-14 13:05:48 +0900

    [Update] Enum.cs
    열거형을 클래스 밖으로 뺌

--- proj/Enum.cs
+++ proj/Enum.cs
@@ -6,15 +6,13 @@ using System.Threading.Tasks;
 
 namespace TextRPG
 {
-    // 텍스트 알피지의 열거형만을 담아두는 클래스.
-    internal class Enum
-    {
-        // 사용될 씬 클래스의 유형
-        public enum SceneType { Title, Select, Town, Shop, Inventory, Battle, GameOver, SIZE };
+    // 텍스트 알피지의 열거형만을 담아두는 cs파일.
+    
+    // 사용될 씬 클래스의 유형
+    public enum SceneType { Title, Select, Town, Shop, Inventory, Battle, GameOver, SIZE };
 
-        // 사용될 플레이어 클래스의 유형
-        public enum Job { Warrior, Mage, Rogue, Archor, SIZE };
+    // 사용될 플레이어 클래스의 유형
+    public enum Job { Warrior, Mage, Rogue, Archor, SIZE };
 
-
-    }
+        
 }
